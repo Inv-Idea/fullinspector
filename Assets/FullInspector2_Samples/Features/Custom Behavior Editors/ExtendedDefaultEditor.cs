e49968a3fc03dc421eb87d5f0c197efd8824c194
EvilSpirit <anandamide@mail.ru>
2020-04-09 18:55:36 +0700

    remove all unwanted files

--- Assets/FullInspector2_Samples/Features/Custom Behavior Editors/ExtendedDefaultEditor.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-namespace FullInspector.Samples {
-    [AddComponentMenu("Full Inspector Samples/Other/Extended Default Editor")]
-    public class ExtendedDefaultEditor : BaseBehavior<FullSerializerSerializer> {
-        public int fromDefaultEditor1, fromDefaultEditor2, fromDefaultEditor3;
-    }
-}
--- Assets/FullInspector2_Samples/README.txt
+++
@@ -1,5 +0,0 @@
-## Overview
-
-This repository contains all of the samples for [Full Inspector](jacobdufault.github.io/fullinspector).
-
-The samples are released under the MIT open-source license.
